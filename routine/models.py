3d5a8023b7a74c7db5b88b3ecb0696f041e26ee6
jacktembo <jack@jacktembo.com>
2021-04-15 21:25:47 +0200

    creating teacher model

--- routine/models.py
+++ routine/models.py
@@ -1,13 +1,19 @@
 from django.db import models
-from ..utils import *
+from utils import *
+from teachers.models import Teacher, Supervisor
 
 
 class DutyRota(models.Model):
+    """Duty Rota for a specific day"""
     name = models.CharField(max_length=200)
     year = models.ForeignKey(Year, on_delete=models.CASCADE)
     term = models.ForeignKey(Term, on_delete=models.CASCADE)
     Month = models.ForeignKey(Month, on_delete=models.CASCADE)
-    week_number = models.IntegerField()  # Week number in a term.
-    day = models.DateField()  # The day of the week e.g Monday.
-    teachers = models.ManyToManyField('Teachers On Duty')  # Teachers on duty.
-    supervisors = models.ManyToManyField('supervisors On Duty')
+    # Week number in a term.
+    week_number = models.ForeignKey(Week, on_delete=models.CASCADE)
+    # The day of the week e.g Monday.
+    day = models.ForeignKey(Day, on_delete=models.CASCADE)
+    # Teachers on duty.
+    teachers = models.ManyToManyField('Teachers On Duty', Teacher)
+    supervisors = models.ManyToManyField('supervisors On Duty', Supervisor)
+    
--- teachers/models.py
+++ teachers/models.py
@@ -1,8 +1,24 @@
 from django.db import models
+from utils import Gender
 
 
-class Teacher(models.Model):
+class Person(models.Model):
+    """an abstract person where different persons type will inherit"""
     first_name = models.CharField(max_length=30)
     other_name = models.CharField(required=False, max_length=30)
     last_name = models.CharField(max_length=30)
-    gender = models.CharField
+    gender = models.ForeignKey(Gender, on_delete=models.CASCADE)
+    nrc = models.CharField(max_length=12)
+    birth_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Teacher(Person):
+    phone_number = models.CharField(max_length=14)
+    email = models.EmailField(max_length=64)
+
+
+class Supervisor(Teacher):
+    pass
