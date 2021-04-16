408d7e2ffa3c0d0f4e958e1e5c896dfbea790926
jacktembo <jack@jacktembo.com>
2021-04-16 16:40:42 +0200

    splitting views to make 3 from 1

--- routine/views.py
+++ routine/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, reverse, get_list_or_404, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 
 from datetime import date, datetime, time, timedelta
-from .models import DutyRota
+from .models import DutyRota, Announcement
 from teachers.models import Teacher, Supervisor
 
 
@@ -12,16 +12,27 @@ tomorrow = today + timedelta(days=1)  # Tomorrows's date
 current_year = today.year
 
 def index(request):
+    context = {}
+    return render(request, 'index.html', context)
+
+
+def today_rota(request):
     # Duty rota that has today's date
     today_duty_rota = get_object_or_404(DutyRota, date=today)
     teachers_on_duty = today_duty_rota.teachers.all()
     supervisors_on_duty = today_duty_rota.supervisors.all()
+
+
+def tomorrow_rota(request):
     #Tomorrow's duty rota
     tomorrow_duty_rota = get_object_or_404(DutyRota, date=tomorrow)
     teachers_on_duty_tomorrow = tomorrow_duty_rota.teachers.all()
     supervisors_on_duty_tomorrow = tomorrow_duty_rota.supervisors.all()
 
 
+def annoucements(request):
+    #Announcements to either pupils or staff
+    annoucements = get_list_or_404(Announcement)
 
 
 
