f0f77f119eb9fcbeed71215dab199f7057b8911d
jacktembo <jack@jacktembo.com>
2021-04-16 14:27:23 +0200

    added some initial views for  duty rota

--- routine/views.py
+++ routine/views.py
@@ -1,8 +1,30 @@
-from django.shortcuts import render, reverse
+from django.shortcuts import render, reverse, get_list_or_404, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 
+from datetime import date, datetime, time, timedelta
+from .models import DutyRota
+from teachers.models import Teacher, Supervisor
+
+
+today = date.today()  # Today's date
+tomorrow = today + timedelta(days=1)  # Tomorrows's date
+
+current_year = today.year
 
 def index(request):
-    return HttpResponse('Hello')
+    # Duty rota that has today's date
+    today_duty_rota = get_object_or_404(DutyRota, date=today)
+    teachers_on_duty = today_duty_rota.teachers.all()
+    supervisors_on_duty = today_duty_rota.supervisors.all()
+    #Tomorrow's duty rota
+    tomorrow_duty_rota = get_object_or_404(DutyRota, date=tomorrow)
+    teachers_on_duty_tomorrow = tomorrow_duty_rota.teachers.all()
+    supervisors_on_duty_tomorrow = tomorrow_duty_rota.supervisors.all()
+
+
+
+
+
+
 
 
