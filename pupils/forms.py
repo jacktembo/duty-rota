b9c5b9786ec777af9fabb9b93ad3d215d42b1c23
jacktembo <jack@jacktembo.com>
2021-04-19 10:34:33 +0200

    first proto

--- pupils/forms.py
+++ pupils/forms.py
@@ -2,6 +2,7 @@ from django.forms import ModelForm
 from .models import LeavePermission
 
 
-class PermissionForm(ModelForm):
+class LeavePermissionForm(ModelForm):
     class Meta:
         model = LeavePermission
+        fields = '__all__'
--- pupils/views.py
+++ pupils/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, Http404, HttpResponseRedirect
 from .models import LeavePermission
 from datetime import datetime, date
+from .forms import LeavePermissionForm
 
 
 def index(request):
@@ -9,24 +10,14 @@ def index(request):
 
 
 def permission(request):
+
     if request.method != 'POST':
-        return render(request, 'index.html')
+        form = LeavePermissionForm()
+
+        return render(request, 'leave.html', {'form': form})
     else:
-
-        """Permission request to leave campus"""
-        captured = request.POST  # We capture data from the form.
-        leave_permission = LeavePermission(
-            pupil_name=captured['Name'],
-            pupil_id=564667,
-            pupil_email=captured['Email'],
-            pupil_phone=captured['Number'],
-            reason=captured['SingleLine'],
-            explanation=captured['MultiLine'],
-            date_requested=datetime.today(),
-            date_leaving=date.fromisoformat(captured['Date'][::-1]),
-            time_leaving=datetime.time(
-                int(captured['Time_hours']), int(captured['Time_minutes'])),
-            date_back=date.fromisoformat(captured['DateTime_date'][::-1])
-
-        )
-        leave_permission.save()
+        form = LeavePermissionForm(request.POST)
+        if form.is_valid():
+            form.save()
+        else:
+            return HttpResponse('something went wrong')
