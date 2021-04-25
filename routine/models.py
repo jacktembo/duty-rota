1ff3b62f55c1157bb89f79ed26274c6aee432bdb
jacktembo <jack@jacktembo.com>
2021-04-25 03:07:02 +0200

    Homepage now working

--- routine/models.py
+++ routine/models.py
@@ -6,6 +6,7 @@ from datetime import date
 
 from django.db import models
 
+
 class School(models.Model):
     admin = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
@@ -28,9 +29,6 @@ class Person(models.Model):
     nrc = models.CharField(max_length=12)
     birth_date = models.DateField()
     picture = models.ImageField(blank=True, null=True)
-
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}"
 
 
 class TeacherProfile(Person):
